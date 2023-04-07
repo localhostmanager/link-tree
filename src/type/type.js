e1df76745ccc4ce1bb6fe7f3febe7cbbb3d4507f
Haseeb <haseebzafar98@gmail.com>
2023-04-07 07:28:26 +0500

    updates

--- src/type/type.js
+++ src/type/type.js
@@ -1,6 +1,6 @@
 import Typed from 'typed.js';
 var typed = new Typed('.multiText', {
-    strings: ["FullStack Developer", "Web Developer", "UI/UX Designer", "Freelancer"],
+    strings: ["FullStack Developer", "Application Developer", "Backend Developer"],
     loop: true,
     typeSpeed: 100,
     backSpeed: 80,
